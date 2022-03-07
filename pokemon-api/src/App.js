ea508a0e72bab64bcb1da6e880cd640fae8c5c84
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-03-07 16:26:21 +1100

    added code to App.js

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 // components
 import Navbar from './components/Navbar';
@@ -9,8 +11,24 @@ import SinglePokemon from './Pages/SinglePokemon';
 
 
 function App() {
-  
-  
+  const [arrayOfPokemon, setArrayOfPokemon] = useState();
+
+  const pushAllPokemonToArray = async () => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+    const data = response.data.results;
+    // console.log(data);
+    let pokemonNames = [];
+    data.forEach(({name}) => {
+      pokemonNames.push(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    })
+    console.log([...pokemonNames]);
+    setArrayOfPokemon(pokemonNames)
+  }
+
+  useEffect(() => {
+    pushAllPokemonToArray()
+  }, [])
+  // console.log(arrayOfPokemon);
   return (
     <Router>
       <div>
@@ -19,6 +37,15 @@ function App() {
           <Route path="/" element={ <AllPokemon /> } />
           <Route path="/pokemon" element={ <SinglePokemon /> } />
         </Routes>
+
+        {/* {
+          arrayOfPokemon.map((pokemon) => (
+            <div>
+              <h1>{pokemon}</h1>
+            </div>
+          ))
+
+        } */}
 
       </div>
     </Router> 
