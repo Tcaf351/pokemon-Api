60c1f6237a50cf41fb74e5ad600cfa6d43429201
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-03-09 13:00:51 +1100

    used flexbox to center content

--- pokemon-api/src/components/Card.jsx
+++ pokemon-api/src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
  const { pokemon } = props;
 
     return ( 
-        <div key={pokemon.name}>
+        <div key={pokemon.name} className="text-xl flex items-center justify-center" >
             <h1>{pokemon.name}</h1>
             <img src={pokemon.sprites.front_default} alt="pokemon" />
         </div>
