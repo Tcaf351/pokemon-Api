e7e3049f3c87e839f4955f6fbc6e342f5d92a850
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-03-31 17:43:36 +1100

    Added a back button to error page

--- pokemon-api/src/Pages/Error.jsx
+++ pokemon-api/src/Pages/Error.jsx
@@ -1,4 +1,5 @@
-
+// packages
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return ( 
@@ -7,6 +8,7 @@ const Error = () => {
             <div className="flex items-center justify-center">
                 <img className="h-80" src="/gottaCatchThemAll.jpeg" alt="pokemon meme" />
             </div>
+            <Link to="/" className='bg-indigo-500 px-3 py-1 mx-10 my-3 rounded-lg text-gray-100 hover:bg-indigo-700 transition ease-out duration-200'><button>Go Back</button></Link>
         </div>
      );
 }
